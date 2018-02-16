1a8fcfba96d87a4cc704b33beca5259f99bf1c82
podoprigoraisv <podoprigoraisv@gmail.com>
2018-02-16 15:43:21 +0200

    Forming up the MainPage

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import MainLayout from "../components/MainLayout";
-import Home from "./Home";
+import MainPage from "./MainPage";
 
 import "../../stylesheet/components/components.scss";
 
 const App = () => (
   <MainLayout>
-    <Route path="/" component={Home} />
+    <Route path="/" component={MainPage} />
   </MainLayout>
 );
 
