2df824640dcdb4905221f3cc35cee2b7c006f86f
podoprigoraisv <podoprigoraisv@gmail.com>
2018-02-22 01:47:20 +0200

    Added Footer to MainLayout

---
+++ src/components/MainLayout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default props => (
+  <div className="main__footer">
+    <div className="resp-content">
+      <div className="copyright">© {new Date().getFullYear()} React Shop</div>
+    </div>
+  </div>
+);
