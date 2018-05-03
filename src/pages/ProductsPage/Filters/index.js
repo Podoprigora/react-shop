ad2522c459e6644456925341bcfa522bccae2ae7
podoprigoraisv <podoprigoraisv@gmail.com>
2018-05-03 14:08:44 +0300

    Changed OptionsList, added size option to limit render items

--- src/pages/ProductsPage/Filters/index.js
+++ src/pages/ProductsPage/Filters/index.js
@@ -4,8 +4,11 @@ import classNames from "classnames";
 
 import withPreventScrollingOfParentElement from "../../components/ui/helpers/withPreventScrollingOfParentElement";
 import CollapsiblePanel from "../../components/ui/CollapsiblePanel";
-import OptionsList, { CheckboxOption } from "../../components/ui/OptionsList";
+import OptionsList, { CheckboxOption, OptionItem } from "../../components/ui/OptionsList";
 import brandsData from "../../../../data/brands";
+import categoriesData from "../../../../data/categories";
+
+const categoriesItems = categoriesData[0].items[1].items;
 
 class ProductFilters extends React.PureComponent {
   static propTypes = {
@@ -19,17 +22,23 @@ class ProductFilters extends React.PureComponent {
 
     return (
       <aside className="product-filters" ref={elRef} onWheel={onMouseWheel}>
+        <CollapsiblePanel header="Categories">
+          <OptionsList size={10}>
+            {categoriesItems.map((item, index) => (
+              <OptionItem key={index} value={index} iconCls="icon-keyboard_arrow_right">
+                {item.title}
+              </OptionItem>
+            ))}
+          </OptionsList>
+        </CollapsiblePanel>
         <CollapsiblePanel header="Brands">
-          {brandsData &&
-            brandsData.length && (
-              <OptionsList selMode="multi">
-                {brandsData.map((brand, index) => (
-                  <CheckboxOption key={index} value={brand.name}>
-                    {brand.title}
-                  </CheckboxOption>
-                ))}
-              </OptionsList>
-            )}
+          <OptionsList selMode="multi" size={10}>
+            {brandsData.map((brand, index) => (
+              <CheckboxOption key={index} value={brand.name}>
+                {brand.title}
+              </CheckboxOption>
+            ))}
+          </OptionsList>
         </CollapsiblePanel>
       </aside>
     );
