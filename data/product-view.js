a66ff3e51b7ea835ddc88c99388f88d3572b1f17
podoprigoraisv <podoprigoraisv@gmail.com>
2018-07-08 13:42:47 +0300

    Update

---
+++ data/product-view.js
@@ -0,0 +1,38 @@
+export default {
+  id: 12468,
+  name: "Family Biz - Hooded Jacket for Women - Grey",
+  brand: "NAKETANO",
+  oldPrice: 74.95,
+  specialPrice: 67.47,
+  currency: "EUR",
+  images: [
+    "data/images/product/naketano-family-biz-hooded-jacket-women-grey.jpg",
+    "data/images/product/naketano-family-biz-hooded-jacket-women-grey2.jpg",
+    "data/images/product/naketano-family-biz-hooded-jacket-women-grey3.jpg"
+    // "data/images/product/BY004EWBUAG2_6855317_1_v2.jpg"
+  ],
+  sizes: ["XS", "S", "M", "L"],
+  features: [
+    "Regular fit",
+    "Large hood, Double-layer hood, Drawstring hood",
+    "High collar",
+    "Asymmetric zip, Zip with a logo zipper pull",
+    "Side pockets",
+    "Ribbed sleeve cuffs",
+    "Ribbed waistband",
+    "Mottled look",
+    "Logo emblem, Logo patch",
+    "Sweat material, Warming material",
+    "Washing instructions: Machine washable at 30°C, Do not tumble dry, Iron at a low temperature",
+    "Contains non-textile parts of animal origin: No",
+    "Manufacturer colour: grey melange"
+  ],
+  material: "Fabric: 65% Cotton, 35% Polyester",
+  modelSize: {
+    Size: "S",
+    "Breast x waist x hips": "82 x 60 x 88 cm",
+    Height: "172 cm"
+  },
+  rating: 5,
+  comments: 2
+};
