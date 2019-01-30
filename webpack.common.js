ce0036cb20e4e1fcdeeca27d7bce3c5b30c9e9c4
podoprigoraisv <podoprigoraisv@gmail.com>
2019-01-30 17:33:58 +0200

    Added a favicon

--- webpack.common.js
+++ webpack.common.js
@@ -72,7 +72,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "public/index.html"
+            template: "public/index.html",
+            favicon: "public/favicon.ico"
         })
     ]
 };
